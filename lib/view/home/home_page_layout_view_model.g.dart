ba0adba006a9126548a8f75f855cc9383a621454
8848man <83057037+8848man@users.noreply.github.com>
2024-02-01 17:56:40 +0900

    --커밋

--- lib/view/home/home_page_layout_view_model.g.dart
+++ lib/view/home/home_page_layout_view_model.g.dart
@@ -7,12 +7,12 @@ part of 'home_page_layout_view_model.dart';
 // **************************************************************************
 
 String _$homePageLayoutPageHash() =>
-    r'cb22ba6ef6a8fbb3e0f652c1bfa3e5504cac15dd';
+    r'fe7c790e34346d03714c5db06d0203255059af5d';
 
 /// See also [HomePageLayoutPage].
 @ProviderFor(HomePageLayoutPage)
-final homePageLayoutPageProvider = AutoDisposeAsyncNotifierProvider<
-    HomePageLayoutPage, HomePageModelData>.internal(
+final homePageLayoutPageProvider =
+    AsyncNotifierProvider<HomePageLayoutPage, HomePageModelData>.internal(
   HomePageLayoutPage.new,
   name: r'homePageLayoutPageProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -22,6 +22,6 @@ final homePageLayoutPageProvider = AutoDisposeAsyncNotifierProvider<
   allTransitiveDependencies: null,
 );
 
-typedef _$HomePageLayoutPage = AutoDisposeAsyncNotifier<HomePageModelData>;
+typedef _$HomePageLayoutPage = AsyncNotifier<HomePageModelData>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
